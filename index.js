6350027dcd336f3cfcd2da6ac52d8edeffa9a5b7
Jay Pan <lb0104672@gmail.com>
2023-06-04 17:00:05 +0800

    [feat]: lambda generate variant images

---
+++ index.js
@@ -0,0 +1,77 @@
+const {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+} = require("@aws-sdk/client-s3");
+const sharp = require("sharp");
+
+const s3 = new S3Client({
+  region: "ap-northeast-3",
+  credentials: {
+    accessKeyId: process.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  },
+});
+
+exports.handler = async (event) => {
+  const bucket = event.Records[0].s3.bucket.name;
+  const key = decodeURIComponent(
+    event.Records[0].s3.object.key.replace(/\+/g, " ")
+  );
+
+  console.log("bucket", bucket);
+  console.log("object key", key);
+
+  try {
+    const getObjectCommand = new GetObjectCommand({
+      Key: key,
+      Bucket: bucket,
+    });
+    const response = await s3.send(getObjectCommand);
+    const stream = response.Body;
+    const filename = key.split("/")[2];
+    const imageExtensionsRegex = /\.(jpg|jpeg|png|gif|bmp|svg|heic)$/i;
+    const isImage = imageExtensionsRegex.test(filename);
+
+    if (!isImage) {
+      console.log("object is not an image: filename", filename);
+      return;
+    }
+
+    const buffer = await new Promise((resolve, reject) => {
+      const chunks = [];
+
+      stream.on("data", (chunk) => chunks.push(chunk));
+      stream.once("end", () => resolve(Buffer.concat(chunks)));
+      stream.once("error", reject);
+    });
+
+    const smallImage = await sharp(buffer).resize(400).toBuffer();
+    const smallImageKey = `places/small/small-${filename}`;
+    const putSmallImageCommand = new PutObjectCommand({
+      Bucket: bucket,
+      Key: smallImageKey,
+      Body: smallImage,
+    });
+
+    const mediumImage = await sharp(buffer).resize(700).toBuffer();
+    const mediumImageKey = `places/medium/medium-${filename}`;
+    const putMediumImageCommand = new PutObjectCommand({
+      Bucket: bucket,
+      Key: mediumImageKey,
+      Body: mediumImage,
+    });
+
+    await Promise.all([
+      s3.send(putSmallImageCommand),
+      s3.send(putMediumImageCommand),
+    ]);
+    console.log("Variant image created and stored successfully!");
+  } catch (err) {
+    console.error("Error:", error);
+    return {
+      statusCode: 500,
+      body: "Error processing the image.",
+    };
+  }
+};
---
+++ readme.md
@@ -0,0 +1,11 @@
+## Caveat
+
+```
+npm install --platform=linux --arch=x64 sharp
+```
+
+## Deploy
+
+```
+npm run zip
+```
